083fa153aa8b64bbb977307e3701da705ec6bee0
Juls <152922186+julsprats@users.noreply.github.com>
2024-02-14 21:57:20 -0500

    Add files via upload

--- GROUP8_Tourism/ActivitiesScreen.swift
+++ GROUP8_Tourism/ActivitiesScreen.swift
@@ -7,14 +7,27 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ActivitiesScreen: View {
+    @State private var searchText = ""
     let activities = Singleton.shared.getActivities()
+    
+    var filteredActivities: [Activity] {
+        if searchText.isEmpty {
+            return activities
+        } else {
+            return activities.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
     
     var body: some View {
         NavigationView {
             VStack {
+                SearchBar(text: $searchText)
+                    .padding(.horizontal)
                 
-                List(activities) { activity in
+                List(filteredActivities) { activity in
                     NavigationLink(destination: ActivityDetails(activity: activity)) {
                         ActivityRow(activity: activity)
                     }
@@ -31,6 +44,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
 #Preview {
     ContentView()
